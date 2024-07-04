d0a020f4eee9b797de01bbff844c07b90012fc1d
MasterCaxaRok2023 <kykloBodwow@gmail.com>
2024-07-04 18:54:11 +0300

    Добавьте файлы проекта.

---
+++ ToDo/Models/ToDo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace ToDoDemo.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Добавьте описание")]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Добавьте дату")]
+
+        public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "Выберете категорию")]
+
+        public string CategoryId { get; set; } = string.Empty;
+
+        [ValidateNever]
+        public Category Category { get; set; } = null!;
+
+        [Required(ErrorMessage = "Добавьте статус")]
+        public string StatusId { get; set; } = string.Empty;
+        [ValidateNever]
+        public Status Status { get; set; } = null!;
+
+        public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
+    }
+}
